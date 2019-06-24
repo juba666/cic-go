da63ec5cd4f6aa39fb999351c575631af8a50500
juba666 <gniruban88@gmail.com>
2019-06-24 16:46:50 +0100

    Update cic.go

--- cic.go
+++ cic.go
@@ -4,18 +4,63 @@ import (
 "math"
 )
 func main() {
+fmt.Println("CHOOSE FROM THE FOLLOWING OPTIONS:\n"+
+"                           \n"+
+"1.Future Amount Calculator\n"+
+"2.Principal Amount Calculator\n"+
+"3.Interest Rate Calculator\n"+
+"4.Number of Periods Calculator\n")
+  
+const FA = 1
+const PA = 2
+const IR = 3
+const NP = 4
+var CH float64
+fmt.Print("Enter a choice(1-4): ")
+fmt.Scanf("%f\n", &CH)
+if CH == FA {
+        fmt.Println("You have Future Amount Calculator")
+		calcFA()
+    } else {
+        result := "END TEST"
+        fmt.Println(result)
+    }
+}
+func calcFA() {
 var p float64
 var s string
 var n float64
 var r float64
-  fmt.Print("Enter a Principal Amount(£): ")
-  fmt.Scanf("%f\n", &p)
-  fmt.Print("Enter a Type of Period(days-weeks-months-years): ")
-  fmt.Scanf("%s\n", &s)
-  fmt.Print("Enter a Number of Periods("+s+"): ")
-  fmt.Scanf("%f\n", &n)
-  fmt.Print("Enter a Interest Rate(%): ")
-  fmt.Scanf("%f\n", &r)
-   f := math.Pow(float64(r)/100+1, float64(n))*p
-   F := fmt.Sprintf("%.2f", f)
-  fmt.Println("Future Amount:£",F) }
+fmt.Print("Enter a Principal Amount(£): ")
+fmt.Scanf("%f\n", &p)
+fmt.Print("Enter a Type of Period(days-weeks-months-years): ")
+fmt.Scanf("%s\n", &s)
+fmt.Print("Enter a Number of Periods("+s+"): ") 
+fmt.Scanf("%f\n", &n) 
+fmt.Print("Enter a Interest Rate(%): ")
+fmt.Scanf("%f\n", &r)
+f := math.Pow(float64(r)/100+1, float64(n))*p  
+F := fmt.Sprintf("%.2f", f) 
+fmt.Println("Future Amount:£",F)
+}
+func calcPA() {
+var p float64
+var s string
+var n float64
+var r float64
+fmt.Print("Enter a Principal Amount(£): ")
+fmt.Scanf("%f\n", &p)
+fmt.Print("Enter a Type of Period(days-weeks-months-years): ")
+fmt.Scanf("%s\n", &s)
+fmt.Print("Enter a Number of Periods("+s+"): ") 
+fmt.Scanf("%f\n", &n) 
+fmt.Print("Enter a Interest Rate(%): ")
+fmt.Scanf("%f\n", &r)
+f := math.Pow(float64(r)/100+1, float64(n))*p  
+F := fmt.Sprintf("%.2f", f) 
+fmt.Println("Future Amount:£",F)
+}
+func calcIR() {
+}
+func calcNP() {
+}
