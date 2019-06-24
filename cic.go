8721587927b83dea463358d55e9761a135544d12
juba666 <gniruban88@gmail.com>
2019-06-24 05:21:52 +0100

    Update cic.go

--- cic.go
+++ cic.go
@@ -1,33 +1,21 @@
 package main
-
 import (
-
 "fmt"
 "math"
-
 )
-
 func main() {
-
 var p float64
 var s string
 var n float64
 var r float64
-
   fmt.Print("Enter a Principal Amount(£): ")
-    fmt.Scanf("%f\n", &p)
-
-fmt.Print("Enter a Type of Period(days-weeks-months-years): ")
-    fmt.Scanf("%s\n", &s)
-
+  fmt.Scanf("%f\n", &p)
+  fmt.Print("Enter a Type of Period(days-weeks-months-years): ")
+  fmt.Scanf("%s\n", &s)
   fmt.Print("Enter a Number of Periods("+s+"): ")
-    fmt.Scanf("%f\n", &n)
-
+  fmt.Scanf("%f\n", &n)
   fmt.Print("Enter a Interest Rate(%): ")
-    fmt.Scanf("%f\n", &r)
-
+  fmt.Scanf("%f\n", &r)
    f := math.Pow(float64(r)/100+1, float64(n))*p
    F := fmt.Sprintf("%.2f", f)
-  fmt.Println("Future Amount:£",F)
-
-}
+  fmt.Println("Future Amount:£",F) }
