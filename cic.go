57d27be29ce95005901068000c3db6a6bf296690
juba666 <gniruban88@gmail.com>
2019-06-24 16:49:56 +0100

    Update cic.go

--- cic.go
+++ cic.go
@@ -44,21 +44,21 @@ F := fmt.Sprintf("%.2f", f)
 fmt.Println("Future Amount:£",F)
 }
 func calcPA() {
-var p float64
+var f float64
 var s string
 var n float64
 var r float64
-fmt.Print("Enter a Principal Amount(£): ")
-fmt.Scanf("%f\n", &p)
+fmt.Print("Enter a Future Amount(£): ")
+fmt.Scanf("%f\n", &f)
 fmt.Print("Enter a Type of Period(days-weeks-months-years): ")
 fmt.Scanf("%s\n", &s)
 fmt.Print("Enter a Number of Periods("+s+"): ") 
 fmt.Scanf("%f\n", &n) 
 fmt.Print("Enter a Interest Rate(%): ")
 fmt.Scanf("%f\n", &r)
-f := math.Pow(float64(r)/100+1, float64(n))*p  
-F := fmt.Sprintf("%.2f", f) 
-fmt.Println("Future Amount:£",F)
+p := math.Pow(float64(r)/100+1, float64(n))/f  
+P := fmt.Sprintf("%.2f", p) 
+fmt.Println("Principal Amount:£",P)
 }
 func calcIR() {
 }
