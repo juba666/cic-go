16e270d02b17c2381bba08086ee0df785f83bf6a
Ganesh Niruban <gniruban88@gmail.com>
2019-06-25 08:18:58 +0100

    Update cic.go

--- cic.go
+++ cic.go
@@ -10,14 +10,17 @@ fmt.Println("CHOOSE FROM THE FOLLOWING OPTIONS:\n"+
 "2.Principal Amount Calculator\n"+
 "3.Interest Rate Calculator\n"+
 "4.Number of Periods Calculator\n"+
-"5.Periodic Compounding Future Amount Calculator\n")
+"5.Periodic Compounding Future Amount Calculator\n"+
+"6.Effective Annual Interest Rate Calculator"+
+"                           \n")  
 const FA = 1
 const PA = 2
 const IR = 3
 const NP = 4
 const PCFA = 5
+const EAIR = 6
 var CH float64
-fmt.Print("Enter a choice(1-5): ")
+fmt.Print("Enter a choice(1-6): ")
 fmt.Scanf("%f\n", &CH)
 if CH == FA {
         fmt.Println("You have chosen Future Amount Calculator")
@@ -34,8 +37,11 @@ if CH == FA {
 } else if CH == PCFA {
 	fmt.Println("You have chosen Periodic Compounding Future Amount Calculator")
 		calcPCFA()
+} else if CH == EAIR {
+	fmt.Println("You have chosen Effective Annual Interest Rate Calculator")
+		calcEAIR()
 } else {
-	error := "Enter a number between 1 & 5"
+	error := "Enter a number between 1 & 6"
         fmt.Println(error)
 }
 }
@@ -126,4 +132,17 @@ rn := (r/100)/n
 f := math.Pow(float64(rn)+1, float64(n))*p
 F := fmt.Sprintf("%.2f", f) 
 fmt.Println("Periodic Compounding Future Amount:£",F)
+}
+func calcEAIR() {
+var r float64
+var n float64
+fmt.Print("Enter a Number of Periods within the year: ") 
+fmt.Scanf("%f\n", &n) 
+fmt.Print("Enter a Interest Rate(%): ")
+fmt.Scanf("%f\n", &r)
+rn := (r/100)/n
+R := math.Pow(float64(rn)+1, float64(n))-1
+e := R*100
+E := fmt.Sprintf("%.3f", e) 
+fmt.Println("Effective Annual Interest Rate:",E,"%")
 }
